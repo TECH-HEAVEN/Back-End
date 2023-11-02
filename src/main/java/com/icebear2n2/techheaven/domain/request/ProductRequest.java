d7fb415c271e26599e9621fd741fd106feae0a21
icebear2n2 <icebear2n2@gmail.com>
2023-11-02 11:01:33 +0900

    feat: 상품 서비스 추가 및 테스트 완료

---
+++ src/main/java/com/icebear2n2/techheaven/domain/request/ProductRequest.java
@@ -0,0 +1,51 @@
+package com.icebear2n2.techheaven.domain.request;
+
+import com.icebear2n2.techheaven.domain.entity.Category;
+import com.icebear2n2.techheaven.domain.entity.Product;
+import com.icebear2n2.techheaven.domain.repository.CategoryRepository;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "상품 생성 및 업데이트 요청 모델")
+public class ProductRequest {
+
+    @Schema(description = "상품 ID")
+    private Long productId;
+    @Schema(description = "카테고리 이름", example = "프로그래밍 도서")
+    private String categoryName;
+
+    @Schema(description = "상품 이름", example = "스프링부트 교과서")
+    private String productName;
+
+    @Schema(description = "상품 가격", example = "20000")
+    private Integer productPrice;
+
+
+    public Product toEntity() {
+        return Product.builder()
+                .productName(productName)
+                .productPrice(productPrice)
+                .build();
+    }
+
+    public void updateProductIfNotNull(Product product, CategoryRepository categoryRepository) {
+        if (this.categoryName != null) {
+            Category category = categoryRepository.findByCategoryName(this.categoryName);
+            if (category != null) {
+                product.setCategory(category);
+            }
+        }
+        if (this.productName != null) {
+            product.setProductName(this.productName);
+        }
+        if (this.productPrice != null) {
+            product.setProductPrice(this.productPrice);
+        }
+    }
+}
+
---
+++ src/main/java/com/icebear2n2/techheaven/domain/response/ProductDetailResponse.java
@@ -0,0 +1,70 @@
+package com.icebear2n2.techheaven.domain.response;
+
+import com.icebear2n2.techheaven.domain.dto.ProductDTO;
+import com.icebear2n2.techheaven.domain.entity.ProductDetail;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "상품 상세 정보 응답 모델")
+public class ProductDetailResponse {
+
+    @Schema(description = "응답 성공 여부", example = "true")
+    private boolean success;
+
+    @Schema(description = "응답 메시지", example = "Success")
+    private String message;
+
+    @Schema(description = "상품 상세 데이터")
+    private ProductDetailData productDetailData;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Schema(description = "상품 상세 데이터 내용")
+    public static class ProductDetailData {
+
+        @Schema(description = "상품 정보")
+        private ProductDTO product;
+
+        @Schema(description = "상품 색상 목록", example = "[\"Red\", \"Blue\"]")
+        private List<String> productColors;
+
+        @Schema(description = "상품 크기 목록", example = "[\"S\", \"M\", \"L\"]")
+        private List<String> productSizes;
+
+        @Schema(description = "재고 수량", example = "[10, 20, 30]")
+        private List<Integer> stockQuantity;
+
+        @Schema(description = "생성일", example = "2023-01-01T10:00:00.000Z")
+        private String createdAt;
+
+        @Schema(description = "수정일", example = "2023-01-10T10:00:00.000Z")
+        private String updatedAt;
+
+        public ProductDetailData(ProductDetail productDetail) {
+            this.product = new ProductDTO(productDetail.getProduct());
+            this.productColors = productDetail.getProductColors() != null ? productDetail.getProductColors() : null;
+            this.productSizes = productDetail.getProductSizes() != null ? productDetail.getProductSizes() : null;
+            this.stockQuantity = productDetail.getStockQuantity();
+            this.createdAt = productDetail.getCreatedAt().toString();
+            this.updatedAt = productDetail.getUpdatedAt().toString();
+        }
+    }
+
+    public static ProductDetailResponse success(ProductDetail productDetail) {
+        return new ProductDetailResponse(true, "Success", new ProductDetailData(productDetail));
+    }
+    public static ProductDetailResponse failure(String message) {
+        return new ProductDetailResponse(false, message, null);
+    }
+}
